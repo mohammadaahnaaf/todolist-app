553c8bc030467cf5f36dcdb5f084d55423cdd5c5
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2022-07-29 05:00:59 +0600

    style: :art: tailwind style init

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='bg-pink-600 h-[100vh]'>Home</div>
+  )
+}
+
+export default Home
---
+++ src/data/Data.jsx
@@ -0,0 +1,60 @@
+const data = [
+    {
+        id: 1,
+        title: '',
+        description: '',
+        createdAt: '',
+        todos: [
+            {
+                id: 1,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+            {
+                id: 2,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+            {
+                id: 3,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: '',
+        description: '',
+        createdAt: '',
+        todos: [
+            {
+                id: 1,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+            {
+                id: 2,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+            {
+                id: 3,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+            {
+                id: 4,
+                title: '',
+                done: false,
+                createdAt: '',
+            },
+        ]
+    },
+]
